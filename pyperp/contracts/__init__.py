3da1e7d5742b69087529de71adc5799c2e3e5f11
arsonwang <37729805+Snooowgh@users.noreply.github.com>
2022-05-20 15:13:09 +0800

    update pool addr

--- pyperp/contracts/__init__.py
+++ pyperp/contracts/__init__.py
@@ -4,3 +4,5 @@ from pyperp.contracts.marketRegistry import *
 from pyperp.contracts.orderBook import *
 from pyperp.contracts.vault import *
 from pyperp.contracts.types import *
+from pyperp.contracts.pairPool import *
+from pyperp.contracts.uniV3Pool import *
