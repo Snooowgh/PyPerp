95b17996130d341e8a9d63f3b8494b69da93b144
Snooowgh <37729805+Snooowgh@users.noreply.github.com>
2022-05-22 23:23:49 +0800

    del log

--- pyperp/contracts/pairPool.py
+++ pyperp/contracts/pairPool.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 from pyperp.providers import ApiProvider
-import logging
 
 
 class PairPool:
@@ -16,15 +15,11 @@ class PairPool:
         provider - an object of class derived from ApiProvider
         '''
         self._provider = provider
-        self.logger = logging.getLogger("PairPool")
-
-        self.logger.info("Loading PairPool contract")
         pair_pool_meta = self._provider.load_meta("vBTC")
         self.pair_pool = self._provider._api.eth.contract(
             address=poolAddr,
             abi=pair_pool_meta["abi"]
         )
-        self.logger.info("PairPool contract loaded")
 
     def get_twap_index_price(self):
         interval = 15 * 60
--- pyperp/contracts/uniV3Pool.py
+++ pyperp/contracts/uniV3Pool.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 from pyperp.providers import ApiProvider
-import logging
 
 
 class UniV3Pool:
@@ -16,15 +15,12 @@ class UniV3Pool:
         provider - an object of class derived from ApiProvider
         '''
         self._provider = provider
-        self.logger = logging.getLogger("UniV3Pool")
 
-        self.logger.info("Loading UniV3Pool contract")
         uniV3_pool_meta = self._provider.load_meta("vBTCvUSDUniswapV3Pool")
         self.uniV3_pool = self._provider._api.eth.contract(
             address=poolAddr,
             abi=uniV3_pool_meta["abi"]
         )
-        self.logger.info("UniV3Pool contract loaded")
         self.pow_constant = 2 ** 96
 
     def get_mark_price(self):
